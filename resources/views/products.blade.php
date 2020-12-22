56b2c429bcf0964ef139fadbc7c8dcf3c6bb3d42
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-22 23:08:09 +0100

    products page ok

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,15 @@
 @extends('partials.common')
 
 @section('content')
-    {{-- content --}}
+    <section>
+        <div class="container-fluid">
+            <div class="row no-gutters">
+                @foreach ($products as $product)
+                    <div class="col-xs-12 col-md-6 col-lg-4">
+                        <img src="{{ $product['src'] }}" class="img-fluid p-1" alt="{{ $product['titolo'] }}">
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
