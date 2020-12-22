1cc8c3d44767183285d667389089a7194a9da9d7
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-22 20:18:48 +0100

    footer ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,24 @@
 @extends('partials.common')
 
 @section('content')
-    <div class=" hero-section container-fluid">
+    <section class=" hero-section container-fluid">
         <div class="row">
             <div class="col-lg-12 hero-image">
-
             </div>
         </div>
-    </div>
+    </section>
+
+    <section>
+        {{-- grid-products --}}
+    </section>
+
+    <section class="motto">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 text-center text-uppercase">
+                    <h2>la molisana: pasta e farina di qualità</h2>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,17 +1,94 @@
 <footer>
-    <div class="container-lg">
-        <div class="row">
-            <div class="col-lg-4">
-                <img src="{{ asset('images/marchio-sito-test.png') }}" alt="la Molisana - logo">
-            </div>
+    <section class="footer-top">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-lg-3">
+                    <img src="{{ asset('images/marchio-sito-test.png') }}" alt="la Molisana - logo">
+                </div>
 
-            <div class="col-lg-4">
+                <ul class="col-lg-3 list-unstyled text-capitalize">
+                    <li class="text-uppercase">pastificio</li>
+                    <li><a href="#">il pastificio</a></li>
+                    <li><a href="#">piano decorticato a pietra</a></li>
+                    <li><a href="#">il molise c'è</a></li>
+                    <li><a href="#">filiera integrata</a></li>
+                    <li><a href="#">100 anni di pasta</a></li>
+                    <li><a href="#">storia della pasta</a></li>
+                    <li><a href="#">spaghetto quadrato</a></li>
+                    <li><a href="#">le persone</a></li>
+                </ul>
 
-            </div>
+                <div class="col-lg-3">
+                    <ul class="list-unstyled text-capitalize">
+                        <li class="text-uppercase">collezione da chef</li>
+                        <li><a href="#">collezione da chef</a></li>
+                        <li><a href="#">grandi cucine</a></li>
+                        <li><a href="#">biologiche</a></li>
+                        <li><a href="#">quadrate</a></li>
+                    </ul>
 
-            <div class="col-lg-4">
+                    <ul class="list-unstyled text-capitalize">
+                        <li class="text-uppercase"><a href="#" class="exeption">limited edition</a></li>
+                    </ul>
+                </div>
 
+                <div class="col-lg-3 text-uppercase company-details">
+                    <p><strong>regione sociale:</strong> la molisana s.p.a</p>
+                    <p><strong>sede legale:</strong> contrada colle delle api, 100/a - 86100 - compobasso (cb)</p>
+                    <p><strong>pec:</strong> lamolisana@pec.it</p>
+                    <p><strong>tel:</strong> +39 0874 4981</p>
+                    <p><strong>fax:</strong> +39 0874 629584</p>
+                    <p>per segnalazioni scrivere a: <strong><a href="#" class="email-important">info@lamolisana.it</a></strong></p>
+                    <p>per contattare l'area commerciale: <strong><a href="#" class="email-important">commerciale@lamolisana.it</a></strong></p>
+                    <p>per contattare l'area export: <strong><a href="#" class="email-important">export@lamolisana.it</a></strong></p>
+                    <p>dati aziendali: <strong><a href="#" class="email-important">leggi di più</a></strong></p>
+                </div>
             </div>
         </div>
-    </div>
+
+        <div class="container-lg">
+            <div class="row">
+                <ul class="offset-lg-3 col-lg-3 list-unstyled text-capitalize">
+                    <li class="text-uppercase">prodotti</li>
+                    <li><a href="#">le clasiche</a></li>
+                    <li><a href="#">le integrali</a></li>
+                    <li><a href="#">le speciali</a></li>
+                    <li><a href="#">le biologiche</a></li>
+                    <li><a href="#">le gluten-free</a></li>
+                    <li><a href="#">le semole</a></li>
+                    <li><a href="#">le extra di lusso</a></li>
+                </ul>
+
+                <ul class="col-lg-3 list-unstyled text-capitalize">
+                    <li class="text-uppercase">news</li>
+                    <li><a href="#">» e-commerce</a></li>
+                    <li><a href="#">» b2b</a></li>
+                    <li><a href="#">» comunicazione</a></li>
+                    <li><a href="#">» scarica i cataloghi</a></li>
+                    <li><a href="#">» rassegna stampa</a></li>
+                    <li><a href="#">» video</a></li>
+                    <li><a href="#">» contatti</a></li>
+                    <li><a href="#">» lavora con noi</a></li>
+                    <li><a href="#">» certificazioni</a></li>
+                    <li><a href="#">» provacy policy</a></li>
+                    <li><a href="#">» credits</a></li>
+                </ul>
+
+                <ul class="col-lg-3 list-unstyled text-capitalize">
+                    <li><a href="#"><img src="{{ asset('images/numero-verde.jpg') }}" alt="numero verde la molisana"></a></li>
+                    <li><a href="#"><img src="{{ asset('images/whatsapp.jpg') }}" alt="whatsup la molisana"></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-bottom">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    {{-- footer image --}}
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
